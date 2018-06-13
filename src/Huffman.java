d2403a6e9afdf32ce8b0c24276980d32c07a54e3
Jonathan Hsu <bit172@gmail.com>
2018-06-12 23:28:10 -0400

    Frequency Table, Priority Queue Working

--- src/Huffman.java
+++ src/Huffman.java
@@ -1,8 +1,64 @@
-
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 public class Huffman {
 	
 	public static void main(String[] args)
 	{	
+		BufferedReader BR = null;
+		int occurence = 0;
+		FrequencyTable FT = new FrequencyTable();
+		
+		try 
+		{
+			 BR = new BufferedReader(new FileReader(args[0]));
+			 
+			 int current = BR.read();
+			 
+			 while(current != -1)
+			 {
+				FT.add(current, occurence);
+				occurence++;
+				current = BR.read();
+			 }
+		}
+		catch(FileNotFoundException e)
+		{
+			System.out.println("File Not Found");
+			System.exit(0);
+		} 
+		catch (IOException e) 
+		{
+			System.out.println("IO Error");
+			System.exit(0);
+		}
+		
+		TreeNode[] farray = FT.getFrequencyArray();
+		
+		int input_size = 0;
+		
+		for(int i = 0; i < 255;i++)
+		{
+			if(farray[i] != null)
+			{
+				input_size++;
+			}
+		}
+		TreeNode[] input = new TreeNode[input_size];
+		
+		for(int i = 0, j=0; i < 255;i++)
+		{
+			if(farray[i] != null)
+			{
+				input[j] = farray[i];
+				j++;
+			}
+		}
+		
+		PriorityQueue PQ = new PriorityQueue(input);
+		PQ.printPQ();
+		
 		
 	}
 	private class Tree
@@ -10,56 +66,33 @@ public class Huffman {
 		TreeNode root;
 	}
 	
-	private class FrequencyTable
-	{
-		FrequencyNode head;
-		int size = 0;
+	private static class FrequencyTable
+	{	// 256 Total ASCII Characters
+		private TreeNode[] FrequencyArray = new TreeNode[256];
 		
-		public FrequencyTable(FrequencyNode H)
+		public FrequencyTable()
 		{
-			head = H;
-			size++;
+			
 		}
 		
-		public void add(char C,int O)
+		public void add(int I,int O)
 		{
-			FrequencyNode T = head;
+			char character = (char) I;
 			
-			while(T.next != null)
+			if(FrequencyArray[I] == null)
 			{
-				if(T.character == C)
-				{
-					T.frequency++;
-					return;
-				}
-				
-				T = T.next;
+				TreeNode N = new TreeNode(O,character);
+				FrequencyArray[I] = N;
 			}
-			
-			FrequencyNode N = new FrequencyNode(C,O);
-			
-			T.next = N;
-			size++;
+			else
+				FrequencyArray[I].frequency++;
 		}
 		
-	}
-	
-	private class FrequencyNode
-	{
-		int frequency;
-		int occurence;
-		char character;
-		FrequencyNode next;
-		
-		public FrequencyNode(char character,int occurence) 
+		public TreeNode[] getFrequencyArray()
 		{
-			this.frequency = 1;
-			this.occurence = occurence;
-			this.character = character;
-			this.next = null;
+			return FrequencyArray;
 		}
-		
-		
+			
 	}
 
 }
